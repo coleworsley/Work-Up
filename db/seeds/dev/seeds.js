4ea02a0df95f8be9d0145115255480553f6839b7
coleworsley <cole.s.worsley@gmail.com>
2017-07-20 10:08:41 -0600

    WIP: make seeds

---
+++ db/seeds/dev/seeds.js
@@ -0,0 +1,41 @@
+const mockData = require('../mockData')
+
+exports.seed = (knex, Promise) => {
+  return Promise.all(
+    [
+      knex('user_exercises').del(),
+      knex('exercises').del(),
+      knex('users').del()
+    ])
+    .then((promiseArr) => {
+      const userTable = promiseArr[2]
+      const exerciseTable = promiseArr[1]
+      const user_exercisesTable = promiseArr[0]
+
+
+      let userPromises = [];
+      let exercisePromises = [];
+      let user_exercisePromises = [];
+
+
+      mockData.exerciseData.forEach(exercise => {
+        exercisePromises.push(mockData.createExercise(knex, exercise));
+      });
+      
+      mockData.userData.forEach(user => {
+        userPromises.push(mockData.createUser(knex, user));
+      });
+
+      mockData.savedExerciseData.forEach(user_exercise => {
+        user_exercisePromises.push(mockData.createSavedExercise(knex, user_exercise));
+      });
+
+
+      return Promise.all (
+        Promise.all(userPromises),
+        Promise.all(exercisePromises),
+        Promise.all(user_exercisePromises)
+      )
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
---
+++ db/seeds/mockData.js
@@ -0,0 +1,74 @@
+const exerciseData = [
+  {
+    id: 1,
+    exercise_name: 'BenchPress',
+    exercise_description: 'Do some bench press shenanigans'
+  },
+  {
+    exercise_name: 'PullUps',
+    exercise_description: 'Do some pull up shenanigans'
+  },
+  {
+    exercise_name: 'Chin UPSSSSS',
+    exercise_description: 'Something Something Something'
+  }
+]
+
+
+const userData = [
+  {
+    id: 1,
+    first_name: 'Batman',
+    last_name: 'Batman',
+    email: 'Bruce@Wayne.org',
+    password: 'BatDaddy123',
+  },
+  {
+    first_name: 'anotherbat',
+    last_name: 'qwerqwef',
+    email: 'Brucaweffwe@Wayne.org',
+    password: 'BatawefaewDaddy123',
+  }
+]
+
+const savedExerciseData = [
+  {
+    id: 1,
+    user_id: 1,
+    exercise_id: 1
+  },
+]
+
+const createExercise = (knex, exercise) => {
+  return knex('exercises').insert({
+    exercise_name: exercise.exercise_name,
+    exercise_description: exercise.exercise_description
+  })
+};
+
+const createUser = (knex, user) => {
+  return knex('users').insert({
+    first_name: user.first_name,
+    last_name: user.last_name,
+    email: user.email,
+    password: user.password
+  })
+};
+
+const createSavedExercise = (knex, workout) => {
+  return knex('user_exercises').insert({
+    user_id: workout.user_id,
+    exercise_id: workout.exercise_id
+  })
+};
+
+
+
+module.exports = {
+  exerciseData,
+  createExercise,
+  userData,
+  createUser,
+  savedExerciseData,
+  createSavedExercise
+}
