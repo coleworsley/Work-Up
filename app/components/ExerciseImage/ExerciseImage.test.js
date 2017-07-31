1589e107c551c56459e12f426958ef4648d62692
coleworsley <cole.s.worsley@gmail.com>
2017-07-30 21:13:06 -0600

    add tests for exercise image

--- app/components/ExerciseImage/ExerciseImage.test.js
+++ app/components/ExerciseImage/ExerciseImage.test.js
@@ -2,6 +2,16 @@ import React from 'react'
 import ExerciseImage from './ExerciseImage';
 import { shallow, mount } from 'enzyme';
 
+
+const resolveAfter3Seconds = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 3000);
+  })
+}
+
+
 describe('ExerciseImage Component', () => {
   it('should render', () => {
     const wrapper = shallow(<ExerciseImage imageUrls={['url1', 'url2']}/>)
@@ -9,7 +19,25 @@ describe('ExerciseImage Component', () => {
     expect(wrapper.length).toEqual(1);
   })
 
-  it('should cycle through images every 2 seconds',() => {
+  it('should render loading text when nothing is passed in', () => {
+    const wrapper = shallow(<ExerciseImage />)
 
+    expect(wrapper.find('.detail-placeholder').text()).toBe('Loading')
   })
+
+  it('should render error text if there are no images', () => {
+    const wrapper = shallow(<ExerciseImage imageUrls={[]}/>)
+
+    expect(wrapper.find('.detail-placeholder').text()).toBe('No images found')
+  })
+
+  it('should cycle through images every 2 seconds', async () => {
+    const wrapper = mount(<ExerciseImage imageUrls={['url1', 'url2']}/>)
+
+    expect(wrapper.find('img').prop('src')).toEqual('url1')
+    await resolveAfter3Seconds()
+    wrapper.update()
+    expect(wrapper.find('img').prop('src')).toEqual('url2')
+  })
+
 })
